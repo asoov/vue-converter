8d3b4d548e6cff85fe878fe14d50af1360302450
Adrian Soovary <adrian.soovary@gmail.com>
2023-09-08 12:40:36 +0200

    feat(frontend): create first draft for dashboard page

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -5,6 +5,9 @@ import "./assets/css/styles.css";
 import router from "./router/index";
 import { Quasar } from 'quasar'
 
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 // Import icon libraries
 import '@quasar/extras/material-icons/material-icons.css'
 
@@ -20,6 +23,7 @@ app
       domain: import.meta.env.VITE_AUTH0_DOMAIN,
       clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
       authorizationParams: {
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
         redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
       },
     })
--- packages/frontend/src/pages/DashboardPage.vue
+++ packages/frontend/src/pages/DashboardPage.vue
@@ -1,10 +1,64 @@
 <template>
   <PageLayout>
+    <div>
+      <h1>Hallo</h1>
+      <div class="row q-gutter-md">
+        <q-card class="col my-card bg-primary text-white">
+          <q-card-section>
+            <div class="text-h4">Token Balance</div>
+          </q-card-section>
 
+          <q-card-section>
+            <div class="text-h2">1000</div>
+          </q-card-section>
+
+          <q-card-actions>
+            <q-btn flat>Get More</q-btn>
+          </q-card-actions>
+        </q-card>
+        <q-card class="col my-card bg-primary text-white">
+          <q-card-section>
+            <div class="text-h4">Token Balance</div>
+          </q-card-section>
+
+          <q-card-section>
+            <div class="text-h2">1000</div>
+          </q-card-section>
+
+          <q-card-actions>
+            <q-btn flat>Get More</q-btn>
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
   </PageLayout>
 </template>
 
 
 <script setup lang="ts">
 import PageLayout from '@/components/PageLayout.vue'
+import { useAuth0 } from '@auth0/auth0-vue';
+import axios from 'axios'
+import { ref } from 'vue';
+
+const loading = ref(true)
+const auth0 = useAuth0()
+const getCustomerData = async () => {
+  const accessToken = await auth0.getAccessTokenSilently()
+  const data = await axios.post('http://localhost:9000/customer/get-customer', {
+
+  }, {
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+    }
+  })
+  console.log(data)
+}
+try {
+  getCustomerData()
+} catch (error) {
+
+} finally {
+  loading.value = false
+}
 </script>
