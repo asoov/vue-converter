ccc2c6f8c828997778d36315e5a96374c6f025d3
Adrian Soovary <adrian.soovary@gmail.com>
2023-09-10 12:48:04 +0200

    chore(utils): add dynamoDB table value enum

---
+++ packages/utils/enums/dynamoDb.ts
@@ -0,0 +1,3 @@
+export enum DynamoValues {
+  TableName = 'VueConverterTable2'
+}
--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -8,4 +8,5 @@ export { GenerateSingleVue3FileResponse } from "./types/GenerateSingleVue3FileRe
 export { GenerateSingleVue3FileRequest } from "./types/GenerateSingleVue3FileRequest"
 export { GenerateMultipleVue3FilesResponse } from "./types/GenerateMultipleVue3FilesResponse"
 export { GenerateMultipleVue3FilesRequest } from "./types/GenerateMultipleVue3FilesRequest"
+export { DynamoValues } from "./enums/dynamoDb"
 
