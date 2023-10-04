1dce46714345c870cb2860d69aa8aecd2d2e03ce
Adrian Soovary <adrian.soovary@gmail.com>
2023-10-04 15:50:12 +0200

    chore: adjust shared Customer class

--- packages/utils/types/Customer.ts
+++ packages/utils/types/Customer.ts
@@ -1,12 +1,10 @@
-import { Process } from "src/customer/entities/customer.entity";
-
 export class Customer {
   id: string;
   firstName: string;
   lastName: string;
   paid: boolean;
   aiCredits: number;
-  finishedProcesses: Array<Process>
+  finishedProcesses: Array<any>
 }
 
 
